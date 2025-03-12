23f46398ddf461070a25694f078f90bfd7558d96
Celeste Aquino <celesteaquino12@gmail.com>
2025-03-12 13:39:42 -0300

    New login update

--- LOGIN/script.js
+++ LOGIN/script.js
@@ -21,4 +21,49 @@ registrarseBtn.onclick = function() {
   window.location.href = "../sign-up/index.html";
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Ancho de la pantalla al cargar:", window.innerWidth);
+
+  function agregarFlecha() {
+    let sectionLeft = document.querySelector('.section-left');
+
+    let flechaExistente = document.querySelector('.flecha');
+    if (flechaExistente) {
+      flechaExistente.remove();
+    }
+
+    if (window.innerWidth <= 767) {  
+      console.log("Agregando flecha porque el ancho es menor o igual a 767px");
+
+      let flecha = document.createElement('i');
+      flecha.classList.add("fa", "fa-angle-double-down", "flecha");
+
+      if (sectionLeft) {
+        sectionLeft.appendChild(flecha);
+        console.log("Flecha agregada correctamente");
+        
+        // no funciona
+        // flecha.addEventListener("click", function () {
+        //   console.log("¡Flecha clickeada! Intentando desplazar...");
+        //   let loginSection = document.getElementById("loginID");
+        //   if (loginSection) {
+        //     console.log("Sección encontrada, moviéndose...");
+        //     window.scrollTo({
+        //       top: loginSection.offsetTop - 100,
+        //       behavior: "smooth"
+        //     });
+        //   } else {
+        //     console.log("No se encontró la sección con id 'loginID'");
+        //   }
+        // });
+         
+      }
+    }
+  }
+
+  agregarFlecha();
+  window.addEventListener("resize", agregarFlecha);
+});
+
+
   
